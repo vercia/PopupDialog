8bc98a0f3267ff14fff444cdf7a66ea35704dfba
Kacper Piątkowski <kacper.piatkowskii@gmail.com>
2019-07-17 13:02:05 +0200

    fixed 300 width

--- PopupDialog/Classes/PopupDialogContainerView.swift
+++ PopupDialog/Classes/PopupDialogContainerView.swift
@@ -174,7 +174,7 @@ final public class PopupDialogContainerView: UIView {
             let metrics = ["preferredWidth": preferredWidth]
             constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-(>=40)-[shadowContainer(==preferredWidth@900)]-(>=40)-|", options: [], metrics: metrics, views: views)
         } else {
-            constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-(>=10,==20@900)-[shadowContainer(<=340,>=300)]-(>=10,==20@900)-|", options: [], metrics: nil, views: views)
+            constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-(>=10)-[shadowContainer(==300@900)]-(>=10)-|", options: [], metrics: nil, views: views)
         }
         constraints += [NSLayoutConstraint(item: shadowContainer, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0)]
         centerYConstraint = NSLayoutConstraint(item: shadowContainer, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0)
